0fa74f900d15633cec519b56f206d500810e04f7
hannakaczynska <114800286+hannakaczynska@users.noreply.github.com>
2023-07-19 12:23:46 +0000

    Deploying to gh-pages from @ hannakaczynska/goit-react-hw-08-phonebook@3f264a6de68216236fb26e7e90f48137ba991f7f 🚀

--- static/js/38.edca38aa.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[38],{4038:function(e,t,s){s.r(t);s(2791);var n=s(3329);t.default=function(){return(0,n.jsxs)("form",{children:[(0,n.jsxs)("label",{children:[" E-mail",(0,n.jsx)("input",{type:"text"})]}),(0,n.jsxs)("label",{children:[" Password",(0,n.jsx)("input",{type:"text"})]}),(0,n.jsx)("button",{type:"submit",children:"Login"})]})}}}]);
-//# sourceMappingURL=38.edca38aa.chunk.js.map
